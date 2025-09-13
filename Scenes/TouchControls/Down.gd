df8967ca7115a11bb4f60274b4ede877306373f4
LuizitoxDev <yclobed@gmail.com>
2025-09-13 17:21:02 -0300

    touch shit added

---
+++ Scenes/TouchControls/Down.gd
@@ -0,0 +1,16 @@
+extends TouchScreenButton
+
+# Todas as actions que esse botão deve ativar
+@export var up_actions: Array[String] = ["ui_down", "move_down_0"]
+
+func _ready() -> void:
+	pressed.connect(_on_pressed)
+	released.connect(_on_released)
+
+func _on_pressed() -> void:
+	for act in up_actions:
+		Input.action_press(act)
+
+func _on_released() -> void:
+	for act in up_actions:
+		Input.action_release(act)
